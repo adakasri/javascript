dbc86a3dbab883764bb4c907f44455b1cd0a416d
sriman adaka <adakasriyadav@gmail.com>
2020-12-26 18:47:13 +0530

    printing array elements

---
+++ arrayInFunction/arrayF.js
@@ -0,0 +1,18 @@
+const arrayElements = ['a', 'b', 'c', 'd', 'e'];
+
+const set1 = arrayElements.forEach(function (arrayElements) {
+    console.log(arrayElements);
+});
+
+//--
+
+const elements = ['a', 'b', 'c', 'd', 'e'];
+
+function arrayInFunction(elements) {
+
+    const results = elements.forEach(elements) 
+
+    return elements;
+}
+console.log(elements);
+
