84b1ede8c2d85db698a2c3349e655a1d99f4b58b
sriman adaka <adakasriyadav@gmail.com>
2020-12-26 14:47:35 +0530

    adding

--- addingTwoparameters/addingParameters.js
+++ addingTwoparameters/addingParameters.js
@@ -1,7 +1,11 @@
-let result;
+
 function adding(firstNumber, secondNumber) {
-    result = firstNumber + secondNumber;
+    let result;
+     let addingParmeters=function () {
+        result = firstNumber + secondNumber;
+     }
+     addingParmeters()
+     console.log('adding',result);
     return firstNumber, secondNumber;
 }
 adding(5,5);
-console.log('adding',result);
