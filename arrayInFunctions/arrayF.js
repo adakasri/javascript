e42eee9096277bfe5b189089bd41900d843afe5d
sriman adaka <adakasriyadav@gmail.com>
2020-12-26 18:57:26 +0530

    using for loop also

--- arrayInFunctions/arrayF.js
+++ arrayInFunctions/arrayF.js
@@ -1,3 +1,6 @@
+
+
+
 const arrayElements = ['a', 'b', 'c', 'd', 'e'];
 
 const set1 = arrayElements.forEach(function (arrayElements) {
@@ -16,3 +19,13 @@ function arrayInFunction(elements) {
 }
 console.log(elements);
 
+//
+const elements1 = ['a', 'b', 'c', 'd', 'e'];
+
+function arrayInFunction1(elements1) {
+
+for(let i=0; i<elements1.length; i++)
+
+return elements1;
+}
+console.log(elements1);
